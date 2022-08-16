ecf129d95e8f114404a15cdd3ee749f5eb5fb601
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-08-16 17:03:05 +0100

    radio btn

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import {useSelector} from 'react-redux';
 // Components
 import Header from 'components/Header';
 import Counter from 'components/Counter';
 // Styles
-import styles from './styles';
+import {styles} from './styles';
+import Choice from '../../components/Choice';
 
 export default () => {
+  // Read from store
+  const questions = useSelector(state => state.question.question);
+  const counter = useSelector(state => state.counter.value);
+
+  // Current State
+  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [answers, setAnswers] = useState([]);
+
+  // Life cycle
+  useEffect(() => {
+    setCurrentQuestion(questions[counter]);
+  }, []);
+
+  useEffect(() => {
+    let table = [];
+    if (currentQuestion?.answers === {}) {
+      return;
+    } else {
+      (currentQuestion?.answers || [])?.map(answer => {
+        table = [...table, {...answer, checked: false}];
+      });
+    }
+    setAnswers(table);
+  }, [currentQuestion]);
+
+  // Render line
+  const receiveFromChild = arg => {
+    console.log(arg);
+    let table = answers.map(item =>
+      item?.label === arg
+        ? {...item, checked: !item?.checked}
+        : {...item, checked: false},
+    );
+    setAnswers(table);
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <Counter />
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.title}>Question {currentQuestion?.id}</Text>
+        <Text style={styles.label}>{currentQuestion?.label}</Text>
+        <View style={styles.response}>
+          {answers?.map((answer, index) => (
+            <Choice
+              data={answer}
+              parentCallback={receiveFromChild}
+              statut={answer?.checked}
+              key={index}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- src/modules/Home/styles.js
+++ src/modules/Home/styles.js
@@ -1,8 +1,28 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  content: {
+    flexGrow: 1,
+    paddingTop: '40%',
+    paddingHorizontal: 50,
+  },
+  // title
+  title: {
+    fontWeight: 'bold',
+    color: '#212121',
+    fontSize: 16,
+  },
+  label: {
+    color: '#212121',
+    marginTop: 5,
+    fontWeight: '400',
+  },
+  // response
+  response: {
+    marginTop: 10,
   },
 });
