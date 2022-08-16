39fd8080d8d8162d7990f6ff8593f81f82ef4e10
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-08-16 12:06:17 +0100

    use header comp

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+// Components
+import Header from 'components/Header';
+// Styles
 import styles from './styles';
 
 export default () => {
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <Header />
     </View>
   );
 };
