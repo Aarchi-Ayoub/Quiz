7a8f8b3997177c16c299ccd894c5a749d96f2b9a
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-08-16 12:05:46 +0100

    reducers

---
+++ src/features/questionSlice.js
@@ -0,0 +1,55 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  question: [
+    {
+      id: 1,
+      answers: [
+        {label: 'Rabat', correct: true},
+        {label: 'Casablanca', correct: false},
+      ],
+      image: null,
+      label: 'Capitale du maroc ?',
+      time: 60,
+    },
+    {
+      id: 2,
+      answers: [
+        {label: 'Espagne', correct: true},
+        {label: 'italie', correct: false},
+      ],
+      label:
+        'Dans quel pays peut-on trouver la Catalogne, l’Andalousie et la Castille ?',
+      time: 30,
+    },
+    {
+      id: 3,
+      answers: [
+        {label: 'Vercingétorix', correct: false},
+        {label: 'César', correct: true},
+        {label: 'Attila', correct: false},
+      ],
+      label: 'Qui a dit : « Le sort en est jeté » (Alea jacta est) ?',
+      time: 60,
+    },
+  ],
+};
+
+export const questionSlice = createSlice({
+  name: 'question',
+  initialState,
+  reducers: {
+    setQuestion: state => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.question = data;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {setQuestion} = questionSlice.actions;
+
+export default questionSlice.reducer;
