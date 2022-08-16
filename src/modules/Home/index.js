54ac2e161156086d1c0845d29e74ac0009c72997
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-08-16 17:43:31 +0100

    btn submit

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,18 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, ScrollView} from 'react-native';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 // Components
 import Header from 'components/Header';
 import Counter from 'components/Counter';
+import Button from 'components/Button';
+import Choice from 'components/Choice';
 // Styles
 import {styles} from './styles';
-import Choice from '../../components/Choice';
+// Store Actions
+import {increment} from 'features/counterSlice';
+import {setCorrect} from 'features/questionSlice';
 
-export default () => {
+export default ({navigation}) => {
+  // Redux action
+  const dispatch = useDispatch();
+
   // Read from store
   const questions = useSelector(state => state.question.question);
   const counter = useSelector(state => state.counter.value);
-
+  const length = useSelector(state => state.question.question?.length || 0);
   // Current State
   const [currentQuestion, setCurrentQuestion] = useState({});
   const [answers, setAnswers] = useState([]);
@@ -20,7 +27,7 @@ export default () => {
   // Life cycle
   useEffect(() => {
     setCurrentQuestion(questions[counter]);
-  }, []);
+  }, [counter]);
 
   useEffect(() => {
     let table = [];
@@ -34,9 +41,14 @@ export default () => {
     setAnswers(table);
   }, [currentQuestion]);
 
+  useEffect(() => {
+    answers.filter(answer => {
+      answer?.checked && answer?.correct ? dispatch(setCorrect()) : null;
+    });
+  }, [answers]);
+
   // Render line
   const receiveFromChild = arg => {
-    console.log(arg);
     let table = answers.map(item =>
       item?.label === arg
         ? {...item, checked: !item?.checked}
@@ -45,6 +57,11 @@ export default () => {
     setAnswers(table);
   };
 
+  // Next question
+  const next = () => {
+    if (length > counter + 1) dispatch(increment());
+    else navigation.navigate('Results');
+  };
   return (
     <View style={styles.container}>
       <Header />
@@ -62,6 +79,7 @@ export default () => {
             />
           ))}
         </View>
+        <Button onPress={() => next()} />
       </ScrollView>
     </View>
   );
